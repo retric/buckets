b68bfcab7fecd25bf51f43ce9f4df100a5e77784
retric <retric@gmail.com>
2015-02-27 02:37:36 -0800

    added watcher for changes in template dir

--- app/src/main/buckets.go
+++ app/src/main/buckets.go
@@ -5,15 +5,18 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"gopkg.in/fsnotify.v1"
 )
 
 type MyController struct {
 	AppController
 	page      map[string]string
 	templates map[string]*template.Template
+	includes  []string
 }
 
 /* View Handlers */
@@ -68,15 +71,66 @@ func (c *MyController) initTemplates() {
 	if err != nil {
 		panic(fmt.Sprintf("Error: Unable to fetch templates"))
 	}
+	c.includes = includes
 
-	for _, include := range includes {
+	for _, include := range c.includes {
 		fmt.Printf("loaded %s include from %s\n", filepath.Base(include), include)
 	}
 	for _, file := range files {
-		total := append(includes, file)
+		total := append(c.includes, file)
 		c.templates[filepath.Base(file)] = template.Must(template.ParseFiles(total...))
 		fmt.Printf("loaded %s template from %s\n", filepath.Base(file), file)
 	}
+}
+
+/* Watcher process to fetch template changes */
+func (c *MyController) startWatcher() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(fmt.Sprintf("Error: Unable to init watcher"))
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				// check if file has been modified
+				mod := (event.Op&fsnotify.Create == fsnotify.Create) ||
+					(event.Op&fsnotify.Write == fsnotify.Write) ||
+					(event.Op&fsnotify.Rename == fsnotify.Rename)
+
+				if mod {
+					if filepath.Ext(event.Name) == ".tmpl" {
+						fmt.Printf("Op: %d, file: %s, filepath: %s\n",
+							event.Op, event.Name, filepath.Ext(event.Name))
+						file := event.Name
+						total := append(c.includes, file)
+
+						// wait to reload file as some editors delete/rename
+						time.Sleep(time.Second)
+						fmt.Printf("reloading %s template from %s\n",
+							filepath.Base(file), file)
+						c.templates[filepath.Base(file)] = template.Must(template.ParseFiles(total...))
+						fmt.Printf("reloaded %s template from %s\n",
+							filepath.Base(file), file)
+					}
+				}
+			case err := <-watcher.Errors:
+				fmt.Printf("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("../templates")
+	err2 := watcher.Add("../templates/partials")
+	if err != nil || err2 != nil {
+		panic(fmt.Sprintf("Error: Unable to watch directory"))
+	}
+
+	<-done
 }
 
 /* Main */
@@ -84,6 +138,7 @@ func main() {
 	c := &MyController{page: map[string]string{"Static": "static"}}
 	muxer := mux.NewRouter().StrictSlash(true)
 	c.initTemplates()
+	go c.startWatcher()
 
 	/* Routes */
 	muxer.HandleFunc("/", c.HomeHandler)
