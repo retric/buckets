5ec2b39f11e02fb1a87c32cf143805da7e8bb47b
retric <retric@gmail.com>
2015-04-15 16:40:20 -0700

    upgrade react to v0.13 and impl newbucket form

--- app/static/jsx/components/new.jsx
+++ app/static/jsx/components/new.jsx
@@ -2,19 +2,73 @@ var React = require('react');
 
 var NewBucket = React.createClass({
 
+    getInitialState: function() {
+        return {formOn: false};
+    },
+
     handleClick: function(event) {
-        //TODO: implement server request for new Bucket
+        this.setState({formOn: !this.state.formOn});
+        event.preventDefault();
+    },
+
+    handleBucketSubmit: function(bucketPart) {
+        this.setState({formOn: false});
+        event.preventDefault();
+        $.ajax({
+            url: 'api/buckets/',
+            dataType: 'json',
+            type: 'POST',
+            data: bucketPart,
+            success: function(data) {
+
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error('api/buckets/', status, err.toString());
+            }.bind(this)
+        });
     },
 
     render: function() {
-        return (
-            <div className="bucketCase">
-            <div className="bucket newBucket">
-            <a href="" onClick={this.handleClick}>+</a>
-            </div></div>
-        );
+        if (!this.state.formOn) {
+            return (
+                <div className="bucketCase">
+                <div className="bucket newBucket">
+                <a href="" onClick={this.handleClick}>+</a>
+                </div></div>
+                );
+        } else {
+            return (
+                <NewBucketForm onBucketSubmit={this.handleBucketSubmit} xClick={this.handleClick} />
+            );
+        }
     }
 
+});
+
+var NewBucketForm = React.createClass({
+    handleSubmit: function(event) {
+        event.preventDefault();
+        var name = React.findDOMNode(this.refs.name).value.trim();
+        if (!name) {
+            return;
+        }
+        this.props.onBucketSubmit(JSON.stringify({ name: name }));
+        React.findDOMNode(this.refs.name).value = '';
+    },
+    render: function() {
+        return (
+                <div className="bucketCase">
+                <div className="bucket newBucket">
+                <form className="newBucketForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Bucket name" ref="name" />
+                <p />
+                <input type = "submit" value="Submit" />
+                <input type = "button" value="Cancel" onClick={this.props.xClick} />
+                </form>
+                </div></div>
+
+        );
+    }
 });
 
 module.exports = NewBucket;
